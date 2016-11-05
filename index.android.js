75cb0f4ea9a5fd63abe569ba57f31183b8b5642a
Joe <jwataru@gmail.com>
2016-11-05 20:45:39 +1100

    Simplified sample modal component.

--- index.android.js
+++ index.android.js
@@ -11,80 +11,30 @@
  var {
    AppRegistry,
    Modal,
-   Picker,
    StyleSheet,
-   Switch,
    Text,
    TouchableHighlight,
    View,
  } = ReactNative;
 
- const Item = Picker.Item;
-
- exports.displayName = (undefined: ?string);
- exports.framework = 'React';
- exports.title = '<Modal>';
- exports.description = 'Component for presenting modal views.';
-
- class Button extends React.Component {
-   state = {
-     active: false,
-   };
-
-   _onHighlight = () => {
-     this.setState({active: true});
-   };
-
-   _onUnhighlight = () => {
-     this.setState({active: false});
-   };
-
-   render() {
-     var colorStyle = {
-       color: this.state.active ? '#fff' : '#000',
-     };
-     return (
-       <TouchableHighlight
-         onHideUnderlay={this._onUnhighlight}
-         onPress={this.props.onPress}
-         onShowUnderlay={this._onHighlight}
-         style={[styles.button, this.props.style]}
-         underlayColor="#a9d9d4">
-           <Text style={[styles.buttonText, colorStyle]}>{this.props.children}</Text>
-       </TouchableHighlight>
-     );
-   }
- }
-
- const supportedOrientationsPickerValues = [
-   ['portrait'],
-   ['landscape'],
-   ['landscape-left'],
-   ['portrait', 'landscape-right'],
-   ['portrait', 'landscape'],
-   [],
- ];
-
  class SampleModal extends React.Component {
-   state = {
-     animationType: 'none',
-     modalVisible: false,
-     transparent: false,
-     selectedSupportedOrientation: 0,
-     currentOrientation: 'unknown',
-   };
+   constructor(props){
+     super(props);
+
+     this._onPressButton = this._onPressButton.bind(this);
+
+     this.state = {
+       modalVisible: false,
+     };
+   }
 
    _setModalVisible = (visible) => {
      this.setState({modalVisible: visible});
    };
 
-   _setAnimationType = (type) => {
-     this.setState({animationType: type});
-   };
-
-   _toggleTransparent = () => {
-     this.setState({transparent: !this.state.transparent});
-   };
+   _onPressButton(){
+     this._setModalVisible(true);
+   }
 
    render() {
      var modalBackgroundStyle = {
@@ -93,81 +43,33 @@
      var innerContainerTransparentStyle = this.state.transparent
        ? {backgroundColor: '#fff', padding: 20}
        : null;
-     var activeButtonStyle = {
-       backgroundColor: '#ddd'
-     };
 
      return (
        <View>
          <Modal
-           animationType={this.state.animationType}
-           transparent={this.state.transparent}
+           animationType={'slide'}
            visible={this.state.modalVisible}
            onRequestClose={() => this._setModalVisible(false)}
-           supportedOrientations={supportedOrientationsPickerValues[this.state.selectedSupportedOrientation]}
-           onOrientationChange={evt => this.setState({currentOrientation: evt.nativeEvent.orientation})}
            >
            <View style={[styles.container, modalBackgroundStyle]}>
              <View style={[styles.innerContainer, innerContainerTransparentStyle]}>
                <Text>This modal was presented {this.state.animationType === 'none' ? 'without' : 'with'} animation.</Text>
                <Text>It is currently displayed in {this.state.currentOrientation} mode.</Text>
-               <Button
-                 onPress={this._setModalVisible.bind(this, false)}
-                 style={styles.modalButton}>
-                 Close
-               </Button>
+               <TouchableHighlight onPress={this._setModalVisible.bind(this, false)}>
+                 <Text>Close</Text>
+               </TouchableHighlight>
              </View>
            </View>
          </Modal>
-         <View style={styles.row}>
-           <Text style={styles.rowTitle}>Animation Type</Text>
-           <Button onPress={this._setAnimationType.bind(this, 'none')} style={this.state.animationType === 'none' ? activeButtonStyle : {}}>
-             none
-           </Button>
-           <Button onPress={this._setAnimationType.bind(this, 'slide')} style={this.state.animationType === 'slide' ? activeButtonStyle : {}}>
-             slide
-           </Button>
-           <Button onPress={this._setAnimationType.bind(this, 'fade')} style={this.state.animationType === 'fade' ? activeButtonStyle : {}}>
-             fade
-           </Button>
-         </View>
-
-         <View style={styles.row}>
-           <Text style={styles.rowTitle}>Transparent</Text>
-           <Switch value={this.state.transparent} onValueChange={this._toggleTransparent} />
-         </View>
-
          <View>
-           <Text style={styles.rowTitle}>Supported orientations</Text>
-           <Picker
-             selectedValue={this.state.selectedSupportedOrientation}
-             onValueChange={(_, i) => this.setState({selectedSupportedOrientation: i})}
-             itemStyle={styles.pickerItem}
-             >
-             <Item label="Portrait" value={0} />
-             <Item label="Landscape" value={1} />
-             <Item label="Landscape left" value={2} />
-             <Item label="Portrait and landscape right" value={3} />
-             <Item label="Portrait and landscape" value={4} />
-             <Item label="Default supportedOrientations" value={5} />
-           </Picker>
+           <TouchableHighlight onPress={this._onPressButton}>
+             <Text>Click me!</Text>
+           </TouchableHighlight>
          </View>
-
-         <Button onPress={this._setModalVisible.bind(this, true)}>
-           Present
-         </Button>
        </View>
      );
    }
  }
-
- exports.examples = [
-   {
-     title: 'Modal Presentation',
-     description: 'Modals can be presented with or without animation',
-     render: () => <ModalExample />,
-   },
- ];
 
  var styles = StyleSheet.create({
    container: {
@@ -178,35 +80,6 @@
    innerContainer: {
      borderRadius: 10,
      alignItems: 'center',
-   },
-   row: {
-     alignItems: 'center',
-     flex: 1,
-     flexDirection: 'row',
-     marginBottom: 20,
-   },
-   rowTitle: {
-     flex: 1,
-     fontWeight: 'bold',
-   },
-   button: {
-     borderRadius: 5,
-     flex: 1,
-     height: 44,
-     alignSelf: 'stretch',
-     justifyContent: 'center',
-     overflow: 'hidden',
-   },
-   buttonText: {
-     fontSize: 18,
-     margin: 5,
-     textAlign: 'center',
-   },
-   modalButton: {
-     marginTop: 10,
-   },
-   pickerItem: {
-     fontSize: 16,
    },
  });
 
